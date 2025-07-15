b5b29b3d4832ed022cbcecd030241ac426413ebe
RightsandWrongsgit <emofsnead@gmail.com>
2025-07-15 19:47:11 -0400

    separate config sync conflict between container and local in both settings.php and Platform.app.yaml settings

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -29,7 +29,16 @@ $settings['state_cache'] = TRUE;
 // Set up a config sync directory.
 //
 // This is defined inside the read-only "config" directory, deployed via Git.
-$settings['config_sync_directory'] = '../config/sync';
+// $settings['config_sync_directory'] = '../config/sync';
+
+if (getenv('PLATFORM_PROJECT') !== false) {
+  // Running on Platform.sh
+  $settings['config_sync_directory'] = '../private/config/sync';
+}
+else {
+  // Running locally (e.g. DDEV)
+  $settings['config_sync_directory'] = '../config/sync';
+}
 
 // Config splits are activated based on the environment (local, develop, staged, main).
 $config['config_split.config_split.local']['status'] = FALSE;
